01ad1c12d4159a228b500bacec0f44e4e80be4e9
Poornima Farrar <pfarrar@mbairpfarrar.local>
2025-07-18 09:10:19 -0700

    Add missing React app structure: public directory, index.html, and main app files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NegationAnalyzer from './components/NegationAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <NegationAnalyzer />
+    </div>
+  );
+}
+
+export default App;
