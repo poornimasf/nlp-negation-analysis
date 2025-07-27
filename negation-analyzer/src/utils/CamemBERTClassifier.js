c32f3c9b44f1f5a19e39a1cd1ee27eea1d6c9278
Poornima Farrar <pfarrar@mbairpfarrar.local>
2025-07-27 14:45:46 -0600

    fix: Remove incorrect transformers dependency, use inference API only

--- negation-analyzer/src/utils/CamemBERTClassifier.js
+++ negation-analyzer/src/utils/CamemBERTClassifier.js
@@ -2,7 +2,7 @@ import { HfInference } from '@huggingface/inference';
 
 class CamemBERTClassifier {
     constructor() {
-        this.inference = new HfInference(process.env.REACT_APP_HF_TOKEN);  // Use existing token
+        this.inference = new HfInference(process.env.REACT_APP_HF_TOKEN);
         this.initialized = false;
         this.modelName = 'camembert-base';
     }
